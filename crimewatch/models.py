986db306de260fdb99ed7ec160b40cba730a05ef
Kevin Hudgens <hudgens1073@gmail.com>
2021-06-19 17:53:31 +0900

    Created Post model

--- crimewatch/models.py
+++ crimewatch/models.py
@@ -1,3 +1,16 @@
 from django.db import models
+from django.contrib.auth.models import User
 
-# Create your models here.
+
+class Post(models.Model):
+    title = models.CharField(max_length=200)
+    author = models.ForeignKey(User, on_delete=models.CASCADE)
+    updated_on = models.DateTimeField(auto_now=True)
+    content = models.TextField()
+    created_on = models.DateTimeField(auto_now=True)
+
+    class Meta:
+        ordering = ["-created_on"]
+
+    def __str__(self):
+        return self.title
