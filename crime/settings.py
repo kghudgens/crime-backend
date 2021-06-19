81da4ae65901022dfc792ef844c4e67ae617d950
Kevin Hudgens <hudgens1073@gmail.com>
2021-06-19 18:14:26 +0900

    added DRF and corsheaders apps

--- crime/settings.py
+++ crime/settings.py
@@ -38,6 +38,8 @@ INSTALLED_APPS = [
     "django.contrib.messages",
     "django.contrib.staticfiles",
     "crimewatch.apps.CrimewatchConfig",
+    "corsheaders",
+    "rest_framework",
 ]
 
 MIDDLEWARE = [
