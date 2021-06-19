697e87dcc6c93dc92a282950bb6ea15f42f74ce2
Kevin Hudgens <hudgens1073@gmail.com>
2021-06-19 17:36:28 +0900

    connected project to postgresql database

--- crime/settings.py
+++ crime/settings.py
@@ -76,8 +76,11 @@ WSGI_APPLICATION = "crime.wsgi.application"
 
 DATABASES = {
     "default": {
-        "ENGINE": "django.db.backends.sqlite3",
-        "NAME": BASE_DIR / "db.sqlite3",
+        "ENGINE": "django.db.backends.postgresql_psycopg2",
+        "NAME": "crime",
+        "USER": "postgres",
+        "PASSWORD": "gonzalo2727",
+        "HOST": "localhost",
     }
 }
 
