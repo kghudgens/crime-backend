93b5215d873d66bfe8db9553a387aa96891fc8e3
Kevin Hudgens <hudgens1073@gmail.com>
2021-06-21 19:43:44 +0900

    added view for user adn Profile serializer

--- users/views.py
+++ users/views.py
@@ -1,3 +1,15 @@
 from django.shortcuts import render
+from django.contrib.auth.models import User
+from rest_framework import viewsets
+from .serializers import ProfileSerializer, UserSerializer
+from .models import Profile
 
-# Create your views here.
+
+class ProfileView(viewsets.ModelViewSet):
+    serializer_class = ProfileSerializer
+    queryset = Profile.objects.all()
+
+
+class UserView(viewsets.ModelViewSet):
+    serializer_class = UserSerializer
+    queryset = User.objects.all()
