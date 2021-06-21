bda301130e45f26918bf0d632966c9c5e102cf73
Kevin Hudgens <hudgens1073@gmail.com>
2021-06-21 19:27:41 +0900

    Created model forms for Profile and user models

--- users/forms.py
+++ users/forms.py
@@ -1,3 +1,15 @@
 from django.forms import ModelForm
 from django.contrib.auth.models import User
 from .models import Profile
+
+
+class UpdateUserForm(ModelForm):
+    class Meta:
+        model = User
+        fields = ["username", "first_name", "last_name", "email"]
+
+
+class UpdateProfileForm(ModelForm):
+    class Meta:
+        model = Profile
+        fields = ["occupation", "age", "location"]
