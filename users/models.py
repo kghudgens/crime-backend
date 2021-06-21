6d08fb1463d970f305e7196b7073ffb7b54a7e09
Kevin Hudgens <hudgens1073@gmail.com>
2021-06-21 19:02:01 +0900

    added states the profile

--- users/models.py
+++ users/models.py
@@ -1,8 +1,21 @@
 from django.db import models
 from django.contrib.auth.models import User
+import us
+
+state_choices = []
+
+for v in us.states.STATES:
+    state_tuple = ()
+    state = us.states.lookup(str(v))
+    state_tuple += (state.abbr, str(v))
+    state_choices.append(state_tuple)
 
 
 class Profile(models.Model):
     user = models.OneToOneField(User, on_delete=models.CASCADE)
     occupation = models.CharField(max_length=50)
     age = models.IntegerField()
+    location = models.CharField(max_length=2, choices=state_choices)
+
+    def __str__(self):
+        return self.user
