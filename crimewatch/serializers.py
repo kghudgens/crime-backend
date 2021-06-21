59d41c85c7d88f4ce5601216843f11e31a441060
Kevin Hudgens <hudgens1073@gmail.com>
2021-06-21 17:37:20 +0900

    created serialized that turns data to json

--- crimewatch/serializers.py
+++ crimewatch/serializers.py
@@ -1,2 +1,17 @@
+from django.contrib.auth.models import User
 from rest_framework import serializers
 from .models import Post
+
+
+class PostSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Post
+        fields = (
+            "id",
+            "title",
+            "author",
+            "updated_on",
+            "content",
+            "created_on",
+            "location",
+        )
