7621e23903d24a78a5dcecdff7b81acd3b72ecc7
Kevin Hudgens <hudgens1073@gmail.com>
2021-06-26 19:57:21 +0900

    Create routes to the api endpoints

--- crime/urls.py
+++ crime/urls.py
@@ -2,8 +2,11 @@ from django.contrib import admin
 from django.urls import path, include
 from rest_framework import routers
 from crimewatch import views
+from users import views as userViews
 
 router = routers.DefaultRouter()
 router.register(r"post", views.PostView, "post")
+router.register(r"profileview", userViews.ProfileView, "profileview")
+router.register(r"userview", userViews.UserView, "userview")
 
 urlpatterns = [path("admin/", admin.site.urls), path("api/", include(router.urls))]
